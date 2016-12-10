42f288908f94bb0c49c96555ea74457130f061ae
Nick Hamilton <git@adventjah.net>
2016-12-10 16:34:38 +0000

    mvp

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,43 +1,67 @@
 import Data.Array
 import Control.Monad
 
-type Move = Int
 type Magic = Int
-data Player = Empty | Player Char
+data Player = Empty | X | O 
   deriving (Show,Eq)
+type Move = Int
+type MovePlayer = (Move,Player) 
 
 type Pos = (Int,Int)
 type Play = (Magic,Player)
 type BState = Array Pos Play 
 type PosState = (Pos,Play)
-data Board = Unplayable 
+data Board a = Unplayable 
            | Playable {state :: BState}
-           -- | Finished {state :: BState}
+           | XWon {state :: BState}
+           | OWon {state :: BState}
+           | Drawed {state :: BState}
   deriving (Show,Eq)
 
-type NBoard = Board
+type NBoard = Board Player
 
 maxsize = 3
 empty = (0,Empty)
 validMoves = [1..maxsize]
 
-{--instance Monad Board where
-  return x = Playable [x]
-  Playable a >>= f = return (f a)
-  Unplayable >>= _ = Unplayable
+instance Monad Board where
+  return p = newBoard 
+  b >>= f = undefined 
+
+{-- 
+
+move m b = b >>= (validPlay m) >>= wins 
+--doPlay takes a move and a board, gives a Unplayable or Playable
+--checkWin takes a board and gives a Playable or Finished 
+--board context records who started the play (therefore only need list of places)
 --}
  
 --given a position and a board, return a new board
-move :: Move -> NBoard -> NBoard
---move f b = b >>= f
-move p b@(Playable a) = if (validPlay p b)
-  then Playable $ a // [((posLookup p),(magicLookup p,Player 'X'))]
-  else Unplayable 
+move :: MovePlayer -> NBoard -> NBoard
+move m b@(Playable a) = checkWin (doPlay m b)
 
-validPlay :: Move -> NBoard -> Bool
-validPlay m b@(Playable s) = (playerAt s (posLookup m))==Empty
+doPlay :: MovePlayer -> NBoard -> NBoard 
+doPlay (m,p) b@(Playable s) = case (playerAt s pos) of
+  Empty -> Playable $ s // [(pos,(magicLookup m,p))]
+  _     -> Unplayable
+  where
+    pos = posLookup m 
 
-newBoard :: NBoard
+checkWin :: NBoard -> NBoard
+checkWin b@(Playable s)
+  | length (moves b) == (maxsize^2) = Drawed s 
+  | otherwise = case (whoWon s) of
+      Just X -> XWon s
+      Just O -> OWon s
+      Nothing -> b 
+
+moves :: NBoard -> [Play]
+moves = (filter (\(x,_)->x>0)).elems.state
+
+playerAt :: BState -> Pos -> Player
+playerAt s p  = snd $ s!p 
+
+--newBoard :: NBoard
 newBoard = Playable $ newState [((x,y),empty) | x<-[1..maxsize], y<-[1..maxsize]]
 
 newState :: [PosState]->BState
@@ -50,16 +74,13 @@ magicLookup:: Move -> Magic
 magicLookup i = [8,1,6,3,5,7,4,9,2]!!(i-1)
 
 --reduce this down
-hasWon :: BState -> Maybe Player
-hasWon s 
-  | won (plays $ Player 'X') = Just $ Player 'X'
-  | won (plays $ Player 'Y') = Just $ Player 'Y'
+whoWon :: BState -> Maybe Player
+whoWon s 
+  | won (plays X) = Just X 
+  | won (plays O) = Just O 
   | otherwise = Nothing where
-    won = elem 15 . map sum . filterM (\x -> [True, False]) 
+    won = elem 15 . map sum . filter (\x->3==length x) . filterM (\x -> [True, False]) 
     plays x = map fst $ filter (\a->x==snd a) $ elems s
-
-playerAt :: BState -> Pos -> Player
-playerAt s p  = snd $ s!p 
 
 --hasWon :: BState -> Maybe Player
 {--hasWon s = if (null winners)
@@ -75,3 +96,7 @@ playerAt s p  = snd $ s!p
 
 winningCombos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 --} 
+
+finishedT =  move (7,X) $ move (5,O) $ move (4,X) $ move (2,O) $ move (1,X) newBoard
+drawedT = move (1,O) $ move (7,X) $ move (3,O) $ move (4,X) $ move (6,O) $ move (2,X) $ move (8,O) $ move (9,X) $ move (5,O) newBoard 
+invalidPlayT = move (5,X) $ move (5,O) newBoard
