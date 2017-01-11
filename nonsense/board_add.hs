1632201582b54544ec57d5dcf157a2d76815dae6
Nick Hamilton <git@adventjah.net>
2017-01-11 13:58:33 +0000

    cant eval complex equations

--- nonsense/board_add.hs
+++ nonsense/board_add.hs
@@ -15,6 +15,8 @@ u = undefined
 data S (m :: Nat) = GetState 
 type M = (S 1)
 type E = (S 0)
+type TWO = (S 2)
+
 data Board p1 p2 p3 p4 p5 p6 p7 p8 p9 where
   NewBoard :: Board E E E E E E E E E
   P1 :: Board M E E E E E E E E  
@@ -27,25 +29,46 @@ data Board p1 p2 p3 p4 p5 p6 p7 p8 p9 where
   P8 :: Board E E E E E E E M E  
   P9 :: Board E E E E E E E E M  
 
-class Board_ b
-
-instance Board_ (Board p1 p2 p3 p4 p5 p6 p7 p8 p9) where
-
 data Player = X | O
 data Status = XWon|OWon|Draw|Unfinished
-data BoardState = forall xs os. (Nxs, Board_ os) => BoardState {
+data BoardState p1 p2 p3 p4 p5 p6 p7 p8 p9 = BoardState {
   status::Status,
   firstPlayer::Player,
-  xBoard = Board p1 p2 p3 p4 p5 p6 p7 p8 p9
+--  moves::[Int],
+  board :: Board p1 p2 p3 p4 p5 p6 p7 p8 p9}
+
+type (<>) x y = (x <=? y) ~ False 
 
 addBoard :: Board (S p1) (S p2) (S p3) (S p4) (S p5) (S p6) (S p7) (S p8) (S p9) -> Board (S p1') (S p2') (S p3') (S p4') (S p5') (S p6') (S p7') (S p8') (S p9') -> Board (S (p1+p1')) (S (p2+p2')) (S (p3+p3')) (S (p4+p4')) (S (p5+p5')) (S (p6+p6')) (S (p7+p7')) (S (p8+p8')) (S (p9+p9'))
 addBoard _ _ = u 
 
+-- move this into a constraint type
+-- then make a Not for it, and use that for whoWon as a constraint
 validBoard :: (p1<=1,p2<=1,p3<=1,p4<=1,p5<=1,p6<=1,p7<=1,p8<=1,p9<=1)=> Board (S p1) (S p2) (S p3) (S p4) (S p5) (S p6) (S p7) (S p8) (S p9) -> Board (S p1) (S p2) (S p3) (S p4) (S p5) (S p6) (S p7) (S p8) (S p9) 
 validBoard = id
 
-newGame p = BoardState Unfinished p NewBoard NewBoard 
+newGame p = BoardState Unfinished p NewBoard
 move m b = validBoard $ addBoard m b
 --whoWon :: (3<=(a+b+c))=> Board (M a) (M b) (M c) -> Bool 
 --whoWon _ = True
 
+{-- fails..
+
+    • Couldn't match expected type ‘num0 ^ 2’
+                  with actual type ‘(10 - 1000) * (10 - 1000)’
+--}
+
+sqr :: S num -> S (num * num)
+sqr _ = GetState
+root :: S (num^2) -> S num 
+root _ = GetState
+m10 :: S num -> S (num-10) 
+m10 _ = GetState
+f :: (num<=1) => S num -> S num
+f _ = GetState
+
+play :: S num -> S state -> S ((10^num)+state)
+play _ _ = GetState
+
+--abs :: S num -> S num
+--abs a b = lg 
