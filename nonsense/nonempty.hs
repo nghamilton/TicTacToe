4d8b68a29c1ce8a5105d2dcc9202650fd52f8269
Nick Hamilton <git@adventjah.net>
2017-02-01 13:14:52 +0000

    tidy

--- nonsense/nonempty.hs
+++
@@ -1,8 +0,0 @@
-infixr 5 :|
-data NonEmpty a = a :| [a] deriving (Show)
-
---toList :: NonEmpty a -> [a]
---fromList :: [a] -> NonEmpty a
-head :: NonEmpty a -> a
-head (a :| _) = a
-
--- nonsense/polymoprhic_attempt1.hs
+++
@@ -1,4 +0,0 @@
-s :: Num b => Int -> b
-s n = case (n) of 
-        1 -> 1 :: Double
-        2 -> 2 :: Integer 
